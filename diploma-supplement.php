b590b1cdb4c047f9f292e0f855abc57320b5890a
Didik K <didikunila@gmail.com>
2024-08-01 21:57:03 +0700

    update fungsi

--- diploma-supplement.php
+++ diploma-supplement.php
@@ -9,7 +9,10 @@
 if ( ! defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly
 }
+
+// Fungsi untuk enqueue skrip dan gaya
 function ds_enqueue_scripts() {
+    
     wp_enqueue_style( 'ds-style', plugin_dir_url( __FILE__ ) . 'assets/css/style.css' );
     wp_enqueue_script( 'jquery' );
     wp_enqueue_script( 'list-js', 'https://cdnjs.cloudflare.com/ajax/libs/list.js/2.3.1/list.min.js', array(), null, true );
@@ -17,11 +20,9 @@ function ds_enqueue_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'ds_enqueue_scripts' );
 
-
 // Fungsi untuk menampilkan konten dengan shortcode
 function ds_diploma_supplement_shortcode() {
-    ob_start();
-    include plugin_dir_path( __FILE__ ) . 'includes/diploma-supplement-template.php';
-    return ob_get_clean();
+    include_once plugin_dir_path( __FILE__ ) . 'includes/diploma-supplement-template.php';
+    return listDocSupplement();
 }
 add_shortcode( 'diploma_supplement', 'ds_diploma_supplement_shortcode' );
--- includes/diploma-supplement-template.php
+++ includes/diploma-supplement-template.php
@@ -1,69 +1,74 @@
 <?php
-// URL CSV Google Sheet
-$sheet_id = '119wjg7Z_jjZi0v7tmHpxOLZWaPhbzeV3k9I4daclJdI';
-$sheet_name = 'Data Wisuda';
-$csv_url = "https://docs.google.com/spreadsheets/d/$sheet_id/gviz/tq?tqx=out:csv&sheet=$sheet_name";
+function listDocSupplement() {
+    // URL CSV Google Sheet
+    $sheet_id = '119wjg7Z_jjZi0v7tmHpxOLZWaPhbzeV3k9I4daclJdI';
+    $sheet_name = 'Data Wisuda';
+    $csv_url = "https://docs.google.com/spreadsheets/d/$sheet_id/gviz/tq?tqx=out:csv&sheet=$sheet_name";
 
-// Fetch CSV data
-$response = wp_remote_get($csv_url);
-$data = array();
+    // Fetch CSV data
+    $response = wp_remote_get($csv_url);
+    $data = array();
 
-if (is_array($response) && !is_wp_error($response)) {
-    $csv_body = wp_remote_retrieve_body($response);
-    $temp = tmpfile(); // Create a temporary file to store CSV data
+    if (is_array($response) && !is_wp_error($response)) {
+        $csv_body = wp_remote_retrieve_body($response);
+        $temp = tmpfile(); // Create a temporary file to store CSV data
 
-    fwrite($temp, $csv_body); // Write CSV data to the temporary file
-    fseek($temp, 0); // Rewind the file pointer to the start of the file
+        fwrite($temp, $csv_body); // Write CSV data to the temporary file
+        fseek($temp, 0); // Rewind the file pointer to the start of the file
 
-    // Use fgetcsv to parse the CSV data properly handling multiline fields
-    while (($line = fgetcsv($temp, 0, ',', '"')) !== FALSE) {
-        $data[] = $line;
+        // Use fgetcsv to parse the CSV data properly handling multiline fields
+        while (($line = fgetcsv($temp, 0, ',', '"')) !== FALSE) {
+            $data[] = $line;
+        }
+
+        fclose($temp); // Close the temporary file
     }
 
-    fclose($temp); // Close the temporary file
-}
+    $header = array_shift($data); // Extract header
 
-$header = array_shift($data); // Extract header
-?>
+    ob_start();
+    ?>
+    <div id="diploma-table-container">
+        <input class="search" placeholder="Search" />
+        <button class="sort" data-sort="name">Sort by Name</button>
+        <button class="sort" data-sort="student-id">Sort by Student ID</button>
 
-<div id="diploma-table-container">
-    <input class="search" placeholder="Search" />
-    <button class="sort" data-sort="name">Sort by Name</button>
-    <button class="sort" data-sort="student-id">Sort by Student ID</button>
+        <table id="diplomaTable" class="display">
+            <thead>
+            <tr>
+                    <th>Student ID</th>
+                    <th>Name</th>
+                    <th>Document Number</th>
+                    <th>Graduation Date</th>
+                    <th>Preview</th>
+                </tr>
+            </thead>
+            <tbody class="list">
+                <?php if (!empty($data)): ?>
+                    <?php foreach ($data as $row): ?>
+                        <tr>
+                            <td class="student-id"><?php echo esc_html($row[1]); ?></td> <!-- NPM -->
+                            <td class="name"><?php echo esc_html($row[2]); ?></td> <!-- Nama -->
+                            <td class="document-number"><?php echo esc_html($row[28]); ?></td> <!-- Nomor SKPI -->
+                            <td class="graduation-date"><?php echo esc_html($row[7]); ?></td> <!-- Tanggal Transkrip -->
+                            <td>
+                                <a href="#" class="ds-view-doc" data-file-id="<?php echo esc_attr($row[44]); ?>">Preview</a>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                <?php endif; ?>
+            </tbody>
+        </table>
 
-    <table id="diplomaTable" class="display">
-        <thead>
-        <tr>
-                <th>Student ID</th>
-                <th>Name</th>
-                <th>Document Number</th>
-                <th>Graduation Date</th>
-                <th>Preview</th>
-            </tr>
-        </thead>
-        <tbody class="list">
-            <?php if (!empty($data)): ?>
-                <?php foreach ($data as $row): ?>
-                    <tr>
-                        <td class="student-id"><?php echo esc_html($row[1]); ?></td> <!-- NPM -->
-                        <td class="name"><?php echo esc_html($row[2]); ?></td> <!-- Nama -->
-                        <td class="document-number"><?php echo esc_html($row[28]); ?></td> <!-- Nomor SKPI -->
-                        <td class="graduation-date"><?php echo esc_html($row[7]); ?></td> <!-- Tanggal Transkrip -->
-                        <td>
-                            <a href="#" class="ds-view-doc" data-file-id="<?php echo esc_attr($row[44]); ?>">Preview</a>
-                        </td>
-                    </tr>
-                <?php endforeach; ?>
-            <?php endif; ?>
-        </tbody>
-    </table>
-
-    <ul class="pagination"></ul>
-</div>
-
-<div id="ds-modal" class="ds-modal">
-    <div class="ds-modal-content">
-        <span class="ds-close">&times;</span>
-        <iframe id="ds-modal-iframe" width="100%" height="480"></iframe>
+        <ul class="pagination"></ul>
     </div>
-</div>
+
+    <div id="ds-modal" class="ds-modal">
+        <div class="ds-modal-content">
+            <span class="ds-close">&times;</span>
+            <iframe id="ds-modal-iframe" width="100%" height="480"></iframe>
+        </div>
+    </div>
+    <?php
+    return ob_get_clean();
+}
