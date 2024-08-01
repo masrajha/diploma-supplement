2ba3aad3d59b14ced26ec45faa4502c9f849f189
Didik K <didikunila@gmail.com>
2024-08-01 08:41:38 +0700

    remove DataTables

--- diploma-supplement.php
+++ diploma-supplement.php
@@ -11,11 +11,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Fungsi untuk enqueue skrip dan gaya
+// Fungsi untuk enqueue skrip dan gaya
 function ds_enqueue_scripts() {
     wp_enqueue_style( 'ds-style', plugin_dir_url( __FILE__ ) . 'assets/css/style.css' );
+    wp_enqueue_script( 'jquery' );
     wp_enqueue_script( 'ds-script', plugin_dir_url( __FILE__ ) . 'assets/js/script.js', array('jquery'), null, true );
-    wp_enqueue_style( 'datatables-style', 'https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css' );
-    wp_enqueue_script( 'datatables-script', 'https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js', array('jquery'), null, true );
 }
 add_action( 'wp_enqueue_scripts', 'ds_enqueue_scripts' );
 
